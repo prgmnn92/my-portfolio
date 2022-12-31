a39125e294e8bcc2bb19731a2ff7d83093172e8e
Phillip Pargmann <pargmann92@gmail.com>
2022-12-31 14:50:42 +0100

    added static page generation for dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -3,39 +3,45 @@ import Container from "../components/Container";
 
 import { getAllFormSubmissions } from "../firebase/firebase";
 
-const Dashboard = () => {
-  const [formSubmissions, setFormSubmissions] = useState([]);
-  useEffect(() => {
-    const test = async () => {
-      const fetchedSubmissions = await getAllFormSubmissions();
-      setFormSubmissions(fetchedSubmissions);
-      console.log(fetchedSubmissions);
-    };
-    test();
-    return () => {};
-  }, []);
+const Dashboard = (props) => {
+  const { formSubmissions } = props;
+  const parsedSubmissions = JSON.parse(formSubmissions);
 
   return (
     <div>
       <Container>
-        {formSubmissions
-          .sort(function (a, b) {
-            return b.date.seconds - a.date.seconds;
-          })
-          .map((item, id) => {
-            const date = new Date(item.date.seconds * 1000);
-            return (
-              <div key={id} className="flex">
-                <div className="px-4">{date.toLocaleString()}</div>
-                <div className="px-4">{item.name}</div>
-                <div className="px-4">{item.email}</div>
-                <div className="px-4">{item.message}</div>
-              </div>
-            );
-          })}
+        {parsedSubmissions ? (
+          parsedSubmissions
+            .sort(function (a, b) {
+              return b.date.seconds - a.date.seconds;
+            })
+            .map((item, id) => {
+              const date = new Date(item.date.seconds * 1000);
+              return (
+                <div key={id} className="flex">
+                  <div className="px-4">{date.toLocaleString()}</div>
+                  <div className="px-4">{item.name}</div>
+                  <div className="px-4">{item.email}</div>
+                  <div className="px-4">{item.message}</div>
+                </div>
+              );
+            })
+        ) : (
+          <div>Loading...</div>
+        )}
       </Container>
     </div>
   );
 };
+
+export async function getStaticProps(context) {
+  const fetchedSubmissions = await getAllFormSubmissions();
+  const submissions = JSON.stringify(fetchedSubmissions);
+  return {
+    props: {
+      formSubmissions: submissions,
+    }, // will be passed to the page component as props
+  };
+}
 
 export default Dashboard;
