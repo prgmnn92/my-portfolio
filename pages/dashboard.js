0215a9d24572f22916656dec48fb9c41c9f95bd9
Phillip Pargmann <pargmann92@gmail.com>
2022-12-31 14:36:55 +0100

    added sort function

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -18,18 +18,21 @@ const Dashboard = () => {
   return (
     <div>
       <Container>
-        {formSubmissions.map((item, id) => {
-          const date = new Date(item.date.seconds * 1000);
-          console.log(item.date.seconds);
-          return (
-            <div key={id} className="flex">
-              <div className="px-4">{date.toLocaleString()}</div>
-              <div className="px-4">{item.name}</div>
-              <div className="px-4">{item.email}</div>
-              <div className="px-4">{item.message}</div>
-            </div>
-          );
-        })}
+        {formSubmissions
+          .sort(function (a, b) {
+            return b.date.seconds - a.date.seconds;
+          })
+          .map((item, id) => {
+            const date = new Date(item.date.seconds * 1000);
+            return (
+              <div key={id} className="flex">
+                <div className="px-4">{date.toLocaleString()}</div>
+                <div className="px-4">{item.name}</div>
+                <div className="px-4">{item.email}</div>
+                <div className="px-4">{item.message}</div>
+              </div>
+            );
+          })}
       </Container>
     </div>
   );
